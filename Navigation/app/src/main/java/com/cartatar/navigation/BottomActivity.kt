09a962beef3072a8154dcd0fb48eb740ff392711
sdram58 <sdram58@hotmail.com>
2021-10-17 20:59:48 +0200

    Navigation 3

--- Navigation/app/src/main/java/com/cartatar/navigation/BottomActivity.kt
+++ Navigation/app/src/main/java/com/cartatar/navigation/BottomActivity.kt
@@ -2,10 +2,28 @@ package com.cartatar.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cartatar.navigation.databinding.ActivityBottomBinding
+
+import androidx.navigation.fragment.NavHostFragment
+
+import androidx.navigation.NavController
+import androidx.navigation.ui.NavigationUI
+
 
 class BottomActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityBottomBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottom)
+
+        setContentView(ActivityBottomBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        setSupportActionBar(binding.toolbar);
+
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?)!!.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController);
     }
 }
--- Navigation/app/src/main/java/com/cartatar/navigation/DrawerActivity.kt
+++ Navigation/app/src/main/java/com/cartatar/navigation/DrawerActivity.kt
@@ -1,11 +1,43 @@
 package com.cartatar.navigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.cartatar.navigation.databinding.ActivityDrawerBinding
+import com.google.android.material.navigation.NavigationView
+
 
 class DrawerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDrawerBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_drawer)
+        setContentView(ActivityDrawerBinding.inflate(layoutInflater).also { binding = it }
+            .root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        val navHosFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHosFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            //Top-level destinations
+                R.id.drawer1Fragment,
+                R.id.drawer2Fragment,
+
+            ),
+            drawerLayout
+        )
+
+        NavigationUI.setupWithNavController(navView, navController)
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration)
+
     }
 }
