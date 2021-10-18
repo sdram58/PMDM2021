856b22203bf35f0c0928d0f718b40dd3affa795c
sdram58 <sdram58@hotmail.com>
2021-10-18 11:45:23 +0200

    Navigation_end

--- Navigation/app/src/main/java/com/cartatar/navigation/TabbedActivity.kt
+++ Navigation/app/src/main/java/com/cartatar/navigation/TabbedActivity.kt
@@ -2,12 +2,16 @@ package com.cartatar.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class TabbedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tabbed)
 
-        title = "Tabbed Activity"
+        val toolBar:Toolbar = findViewById(R.id.toolbar)
+        toolBar.title = "Tabbed Activity"
+        setSupportActionBar(toolBar)
+
     }
 }
