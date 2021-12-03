4bb2da26e7e57b9df05b6abc596d2e9dd4ad7ee7
sdram58 <sdram58@hotmail.com>
2021-12-03 01:59:00 +0100

    Modified_Room_singleton

--- TaskNotesRoom/app/src/main/java/com/catata/tasknotesroom/database/TaskDatabase.kt
+++ TaskNotesRoom/app/src/main/java/com/catata/tasknotesroom/database/TaskDatabase.kt
@@ -1,10 +1,20 @@
 package com.catata.tasknotesroom.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.catata.tasknotesroom.database.entities.TaskEntity
 
 @Database(entities = arrayOf(TaskEntity::class), version = 1)
 abstract class TasksDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
+
+    companion object{ //Singleton Pattern
+        private var instance:TaskDao? = null
+
+        fun getInstance(context: Context):TaskDao{
+            return instance ?: Room.databaseBuilder(context, TasksDatabase::class.java, "tasks-db").build().taskDao().also { instance = it }
+        }
+    }
 }
